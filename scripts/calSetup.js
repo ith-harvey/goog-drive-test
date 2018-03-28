1a60ecbafeef0cee7ad3d07f86d857994c794552
@ianh <ith.harvey@gmail.com>
2018-03-27 22:28:40 -0500

    updating

--- scripts/calSetup.js
+++ scripts/calSetup.js
@@ -1,5 +1,6 @@
 
-
+// jscs:disable requireSemicolons
+// jscs:disable maximumLineLength
 // @doge calendar setup and calendar feature script
 
 /* First things first set your environment variables in the root folder:
@@ -19,7 +20,6 @@
 *
 */
 
-
 const momentTZ = require('moment-timezone');
 const moment = require('moment');
 const rp = require('request-promise')
@@ -29,76 +29,77 @@ const getTodaysDate = currentTimeZone => moment.utc()
 const minutesOfDay = m => m.minutes() + m.hours() * 60;
 
 function formatDate(icalStr) {
-  let strYear = icalStr.substr(0,4);
-  let strMonth = parseInt(icalStr.substr(4,2),10) - 1;
-  let strDay = icalStr.substr(6,2);
-  let strHour = icalStr.substr(9,2);
-  let strMin = icalStr.substr(11,2);
-  let strSec = icalStr.substr(13,2);
-  let dateNeedsFormat = new Date(strYear,strMonth, strDay, strHour, strMin, strSec)
-  return  moment(dateNeedsFormat, "YYYY-MM-DD HH:mm:ss Z")
+  let strYear = icalStr.substr(0, 4);
+  let strMonth = parseInt(icalStr.substr(4, 2), 10) - 1;
+  let strDay = icalStr.substr(6, 2);
+  let strHour = icalStr.substr(9, 2);
+  let strMin = icalStr.substr(11, 2);
+  let strSec = icalStr.substr(13, 2);
+  let dateNeedsFormat = new Date(strYear, strMonth, strDay, strHour, strMin, strSec)
+  return moment(dateNeedsFormat, 'YYYY-MM-DD HH:mm:ss Z')
 }
 
 function localTime(time, timeZone) {
   let timeClone = moment(time)
-  return timeClone.tz(timeZone).format("DD-MM-YYYY hh:mm:ss")
+  return timeClone.tz(timeZone).format('DD-MM-YYYY hh:mm:ss')
 }
 
 function checkIfUserIsSetup(robot, userId) {
   // input validation function
 
   if (robot.brain.get(userId) === null) {
-    console.log('user has not started the setup process.')
+    //user has not started the setup process.
     return true
 
   } else if (robot.brain.get(userId).busyCalUrl === undefined) {
-    console.log('user has not provided a URL.')
+    //user has not provided a URL.
     return true
 
   } else if (robot.brain.get(userId).workHrs === undefined) {
-    console.log('user has not provided working hours.')
+    //user has not provided working hours.
     return true
   }
-  return false // user is good to use cal suggest feature
+
+  return false // user is already setup for cal suggest feature
 }
 
 function wrkHrsParse(wrkHrs, timeZone) {
 
   let start = {
-    Hrs: wrkHrs.slice(0,2),
-    Min: wrkHrs.slice(3,5)
+    Hrs: wrkHrs.slice(0, 2),
+    Min: wrkHrs.slice(3, 5),
   }
 
   let end = {
-    Hrs: wrkHrs.slice(8,10),
-    Min: wrkHrs.slice(11,13)
+    Hrs: wrkHrs.slice(8, 10),
+    Min: wrkHrs.slice(11, 13),
   }
 
-  let test = moment(wrkHrs.slice(0, wrkHrs.indexOf('-')),'HH:mm')
+  let test = moment(wrkHrs.slice(0, wrkHrs.indexOf('-')), 'HH:mm')
   .tz(timeZone).hours(start.Hrs).minutes(start.Min)
 
   let returnObj = {
-    start: moment(wrkHrs.slice(0, wrkHrs.indexOf('-')),'HH:mm')
+    start: moment(wrkHrs.slice(0, wrkHrs.indexOf('-')), 'HH:mm')
     .tz(timeZone).hours(start.Hrs).minutes(start.Min).utc(),
 
-    end: moment(wrkHrs.slice(wrkHrs.indexOf('-') + 1 , wrkHrs.length),'HH:mm')
+    end: moment(wrkHrs.slice(wrkHrs.indexOf('-') + 1, wrkHrs.length), 'HH:mm')
     .tz(timeZone).hours(end.Hrs).minutes(end.Min).utc(),
 
-    timeZone: timeZone
+    timeZone: timeZone,
   }
 
   return returnObj
 }
 
-function randomStartTimesArray(availBlockStarts,availBlockEnds, dateAvailRequested) {
+function randomStartTimesArray(availBlockStarts, availBlockEnds, dateAvailRequested) {
 
   function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random()* ( max-min + 1) + min)
+    return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
   let startTime = {
-      Obj : {},
-      Arr : []
+    Obj: {},
+    Arr: [],
   }
 
   let stagingStartTime = randomIntFromInterval(availBlockStarts, availBlockEnds)
@@ -116,76 +117,80 @@ function randomStartTimesArray(availBlockStarts,availBlockEnds, dateAvailRequest
       startTime.Obj[currStartTime] = true
       startTime.Arr.push(moment(dateAvailRequested).hours(currStartTime).minutes(00).seconds(00))
     }
-
   }
+
   return startTime.Arr
 }
 
-
 class RecordAvailability {
+  constructor() {
+    this.lastEventEndTime = 'undefined'
+    this.availabilityArr = []
+  }
 
-	constructor() {
-	  this.lastEventEndTime = 'undefined'
-    this.suggestions = []
- 	}
-
-  set (wrkHrs, eventEnd, eventStart) {
-
-	  if (eventStart === undefined) { // event started before working hours
-			this.lastEventEndTime = eventEnd
+  set(wrkHrs, eventEnd, eventStart) {
+    if (eventStart === undefined) { //event started before working hours
+      this.lastEventEndTime = eventEnd
       return
     }
 
-	  if (this.lastEventEndTime === 'undefined') {
+    if (this.lastEventEndTime === 'undefined') {
       // first event that day && there is gap time between wrkHrs start and eventStart
       this.lastEventEndTime = wrkHrs.start
     }
 
-	  this.findAvailability(wrkHrs, eventStart)
-	  this.lastEventEndTime = eventEnd
+    this.findAvailability(wrkHrs, eventStart)
+    this.lastEventEndTime = eventEnd
   }
 
-	setUntilEndOfWorkDay(wrkHrs) {
-	  this.findAvailability(wrkHrs, wrkHrs.end)
-	}
+  setUntilEndOfWorkDay(wrkHrs) {
+    this.findAvailability(wrkHrs, wrkHrs.end)
+  }
 
-	dayIsFree(wrkHrs, dateAvailRequested) {
+  dayIsFreeAddAvail(wrkHrs, dateAvailRequested) {
+    this.addAvailability(wrkHrs, wrkHrs.start, wrkHrs.end)
+  }
+
+  generateSuggestTimes(wrkHrs, dateAvailRequested) {
     let endTime
 
-	  randomStartTimesArray(wrkHrs.start.hour(),wrkHrs.end.hour(), dateAvailRequested).forEach( startTime => {
-      endTime = moment(startTime).add(1,'hours')
-      this.createSuggestion(wrkHrs, startTime, endTime)
-    })
+    randomStartTimesArray(
+      wrkHrs.start.hour(),
+      wrkHrs.end.hour(),
+      dateAvailRequested).forEach(startTime =>  {
 
-	}
+        endTime = moment(startTime).add(1, 'hours')
+        this.addAvailability(wrkHrs, startTime, endTime)
+      })
+  }
 
-	findAvailability(wrkHrs, eventStart) {
+  findAvailability(wrkHrs, eventStart) {
     let availTime =  moment.duration(eventStart.diff(this.lastEventEndTime))
     availTime = availTime.asMinutes()
-    let suggestionStartPoint = this.lastEventEndTime
+    let availabilityStartPoint = this.lastEventEndTime
 
     for (let i = 1; i <= (availTime / 60); i++) {
       // only create 60 min suggestions
 
-      this.createSuggestion(wrkHrs, suggestionStartPoint, moment(suggestionStartPoint).add(1,'hours'))
+      this.addAvailability(wrkHrs, availabilityStartPoint, moment(availabilityStartPoint).add(1, 'hours'))
 
-      suggestionStartPoint = moment(suggestionStartPoint).add(1, 'hours') //bump suggestionStartPoint an hour
+      availabilityStartPoint = moment(availabilityStartPoint).add(1, 'hours') //bump suggestionStartPoint an hour
     }
-	}
+  }
 
-	createSuggestion(wrkHrs, suggestionStart, suggestionEnd) {
-	  this.suggestions.push({
+  addAvailability(wrkHrs, availStart, availEnd) {
+    this.availabilityArr.push({
 
-	    start: `${wrkHrs.timeZone}: ${localTime(suggestionStart, wrkHrs.timeZone)} UTC: ${suggestionStart}`,
+      start: `${wrkHrs.timeZone}: ${localTime(availStart, wrkHrs.timeZone)} UTC: ${availStart}`,
 
-	    end: `${wrkHrs.timeZone}: ${localTime(suggestionEnd, wrkHrs.timeZone)} UTC: ${suggestionEnd}`
+      end: `${wrkHrs.timeZone}: ${localTime(availEnd, wrkHrs.timeZone)} UTC: ${availEnd}`,
 
-	  })
-	}
+    })
+  }
 
-	get() {
-    return this.suggestions
-	}
+  get() {
+    return this.availabilityArr
+  }
 
 }
 
@@ -202,95 +207,111 @@ class RecordAvailability {
    * @returns Nothing - calls Availability.set() method
 **/
 
-function findAvailabilityOverTime (eventArr, wrkHrs, dateAvailRequested, timeWindow, Availability) {
+/*
+* 1) events that happen on selected day
+*
+*
+*
+*
+*/
 
-  if (timeWindow === 'day') {
-    eventArr.forEach(date => { // loop over event dates
+let findAvailabilityOverTime = function (eventArr, wrkHrs, dateAvailRequested, timeWindow, Availability) {
+
+  return new Promise((resolve, reject) => {
+    let i = 0
+    let date = eventArr[i]
+
+    while (formatDate(date.DTSTART).date() <= dateAvailRequested.date()) {
+      // console.log('in find avail eventdate', formatDate(date.DTSTART))
+      // console.log('in find avail requestdate', dateAvailRequested)
 
       if (formatDate(date.DTSTART).date() === dateAvailRequested.date()) {
-      // events that happen on selected day
-      console.log('same day!', formatDate(date.DTSTART))
+        // events that happen on selected day
+        // console.log('same day!', formatDate(date.DTSTART))
 
         if (minutesOfDay(formatDate(date.DTSTART)) <= minutesOfDay(wrkHrs.start)) {
-        // event start happens before || same time as wrkhrs start
+          // event start happens before || same time as wrkhrs start
 
-    			if (minutesOfDay(formatDate(date.DTEND)) <= minutesOfDay(wrkHrs.start)) {
-          // event end happens before || same time as wrkhrs start
-    			// the entire event happens before working hours
-    			// do nothing -> go to next event
-    			} else {
-    				//event ends during working hours
-    				Availability.set(wrkHrs, formatDate(date.DTEND))
-    			}
-
-    		} else { // event start happens after wrkhrs start
-
-    			if (minutesOfDay(formatDate(date.DTSTART)) > minutesOfDay(wrkHrs.end)) {
-            // event start happens before  wrkhrs end
-    				Availability.set(wrkHrs, formatDate(date.DTEND), formatDate(date.DTSTART))
-            // 2 Args (record whole chunk of availability from start || last event end - end)
+          if (minutesOfDay(formatDate(date.DTEND)) <= minutesOfDay(wrkHrs.start)) {
+            // event end happens before || same time as wrkhrs start
+            // the entire event happens before working hours
+            // do nothing -> go to next event
           } else {
-    			// the entire event happens after working hours
-          // do nothing -> go to next event
+            //event ends during working hours
+            Availability.set(wrkHrs, formatDate(date.DTEND))
           }
 
-    		}
-      } else {
-    	  console.log('Do nothing event doesnt match day requested')
-    	 }
+        } else { // event start happens after wrkhrs start
 
-    })
+          if (minutesOfDay(formatDate(date.DTSTART)) > minutesOfDay(wrkHrs.end)) {
+            // event start happens before  wrkhrs end
+            Availability.set(wrkHrs, formatDate(date.DTEND), formatDate(date.DTSTART))
+
+            // 2 Args (record whole chunk of availability from start || last event end - end)
+          } else {
+            // the entire event happens after working hours
+            // do nothing -> go to next event
+          }
+
+        }
+      } else {
+        // console.log('Do nothing event doesnt match day requested')
+      }
+
+      if (eventArr.length - 1 === i) break
+
+      i++
+      date = eventArr[i]
+    }
 
     Availability.setUntilEndOfWorkDay(wrkHrs)
 
-  	Availability.get().length ? '' : Availability.dayIsFree(wrkHrs, dateAvailRequested)
+    Availability.get().length ? '' : Availability.dayIsFreeAddAvail(wrkHrs, dateAvailRequested)
 
-    console.log('suggestion arr', Availability.get())
-  }
+    return Availability.get()
+  })
 }
 
 module.exports = (robot) => {
 
   let awaitingUrl = false, awaitingWorkHours = false, calSetupDone = false
 
-  robot.respond(/(cal setup)/i, function(msg) {
-    msg.reply("Woof woof! Welcome to the cal setup wizard. \n \n Please enter your fastmail account’s free/busy calendar URL so I can share your availability upon request.\n\n (To access your free/busy calendar URL visit www.fastmail.com/calendar/ while logged in.\n Select the calendar dropdown > settings > calendars > Edit&share > check free/busy information > copy the url > hit save up top > paste URL in chat and hit enter)")
+  robot.respond(/(cal setup)/i, function (msg) {
+    msg.reply('Woof woof! Welcome to the cal setup wizard. \n \n Please enter your fastmail account’s free/busy calendar URL so I can share your availability upon request.\n\n (To access your free/busy calendar URL visit www.fastmail.com/calendar/ while logged in.\n Select the calendar dropdown > settings > calendars > Edit&share > check free/busy information > copy the url > hit save up top > paste URL in chat and hit enter)')
     awaitingUrl = true
   })
 
-  robot.hear(/^(http|https)/i ,function(msg) {
+  robot.hear(/^(http|https)/i, function (msg) {
     if (awaitingUrl) {
-      robot.brain.set(msg.message.user.id, {busyCalUrl: msg.message.text})
+      robot.brain.set(msg.message.user.id, { busyCalUrl: msg.message.text })
 
-      msg.reply("Woof woof! URL was received... \n \n Excellent, now I need to know your preferred working hours when you will be available for meetings. \n \n Please enter them in 24hr format: <HH:mm>-<HH:mm> (e.g. 09:00-17:00)")
+      msg.reply('Woof woof! URL was received... \n \n Excellent, now I need to know your preferred working hours when you will be available for meetings. \n \n Please enter them in 24hr format: <HH:mm>-<HH:mm> (e.g. 09:00-17:00)')
 
       awaitingUrl = false
       awaitingWorkHours = true
     }
   })
 
-  robot.hear(/[0-9][0-9]:[0-5][0-9]-[0-9][0-9]:[0-5][0-9]/i, function(msg) {
+  robot.hear(/[0-9][0-9]:[0-5][0-9]-[0-9][0-9]:[0-5][0-9]/i, function (msg) {
     if (awaitingWorkHours) {
       console.log('in save', robot.brain.get(msg.message.user.id))
 
-      robot.brain.set(msg.message.user.id,{
-        busyCalUrl:robot.brain.get(msg.message.user.id).busyCalUrl,
-        workHrs: msg.message.text
+      robot.brain.set(msg.message.user.id, {
+        busyCalUrl: robot.brain.get(msg.message.user.id).busyCalUrl,
+        workHrs: msg.message.text,
       })
 
-
-      msg.reply("Woof woof! Thank you for completing the cal setup wizard!, you may now use the `@doge cal suggest` feature.")
+      msg.reply('Woof woof! Thank you for completing the cal setup wizard!, you may now use the `@doge cal suggest` feature.')
 
       awaitingWorkHours = false
       calSetupDone = true
     }
   })
 
-
-  robot.respond(/(cal suggest)/i ,function(msg) {
+  robot.respond(/(cal suggest)/i, function (msg) {
 
     if (checkIfUserIsSetup(robot, msg.message.user.id)) {
-    msg.reply("Woof woof! To use the `@doge cal suggest` feature you must first go through the setup wizard. Do so by typing the command `@doge cal setup`.")
+      msg.reply('Woof woof! To use the `@doge cal suggest` feature you must first go through the setup wizard. Do so by typing the command `@doge cal setup`.')
     } else {
 
       rp(robot.brain.get(msg.message.user.id).busyCalUrl)
@@ -299,15 +320,15 @@ module.exports = (robot) => {
         let output = ical2json.convert(response);
 
         let data = {
-          dateArr : output.VCALENDAR[0].VEVENT,
-          timeZone : output.VCALENDAR[0]["X-WR-TIMEZONE"]
+          dateArr: output.VCALENDAR[0].VEVENT,
+          timeZone: output.VCALENDAR[0]['X-WR-TIMEZONE'],
         }
 
         let dayRequested = getTodaysDate()
         let timeWindow = 'day'
         // let timeWindow = 'week'
 
-        msg.reply("Your current Timezone (set at fastmail.com): " + data.timeZone)
+        msg.reply('Your current Timezone (set at fastmail.com): ' + data.timeZone)
 
         let wrkHrsInUTC =  wrkHrsParse(robot.brain.get(msg.message.user.id).workHrs, data.timeZone)
 
@@ -318,14 +339,19 @@ module.exports = (robot) => {
         let suggestString = ''
 
         Availability.get().forEach((suggestion, index) => {
-          suggestString += `${index+1}) ${suggestion.start} \n ${suggestion.end} \n \n`
+          suggestString += `${index + 1}) ${suggestion.start}
+ ${suggestion.end}
+
+`
         })
 
-        msg.reply(`Woof woof! Here are some meeting suggestions for ${dayRequested.format('LL')}:  \n \n` + suggestString)
+        msg.reply(`Woof woof! Here are some meeting suggestions for ${dayRequested.format('LL')}:
+
+` + suggestString)
 
       }).catch((err)=> {
-        msg.reply("in err")
-        console.log('ERROR: ',err)
+        msg.reply('in err')
+        console.log('ERROR: ', err)
         msg.reply(err)
       })
     }
