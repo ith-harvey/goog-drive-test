c3928141e8e9b98fa2accf1038d6f392f61617de
@ianh <ith.harvey@gmail.com>
2018-04-23 23:47:11 -0500

    fixed err

--- scripts/roomChecker.js
+++ scripts/roomChecker.js
@@ -1,13 +1,20 @@
 
 
 module.exports = (robot) => {
+
   robot.receiveMiddleware((context, next, done) => {
 
-    if (context.response.message.user.roomType ==='p' || context.response.message.user.roomType ==='d' ) {
-      //if the room is Private or a DM pass next()
-      next(done)
-    } else {
-      context.response.reply('I\'m only availabile through DM or a private channel. Please try your request again in a direct message with me.')
+    console.log(context.response.message.text.split(' ')[0]);
+
+    if (context.response.message.text.split(' ')[0] === '@doge') {
+
+      if (context.response.message.user.roomType ==='p' || context.response.message.user.roomType ==='d' ) {
+        //if the room is Private or a DM pass next()
+        next(done)
+      } else {
+        context.response.reply('I\'m only availabile through DM or a private channel. Please try your request again in a direct message with me.')
+      }
     }
+
   })
 }
