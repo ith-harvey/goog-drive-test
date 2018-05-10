9e0f4a1e6f30fc4da5b37472e65dd2ded722f8ff
@ianh <ith.harvey@gmail.com>
2018-05-10 11:21:57 -0500

    refactored

--- scripts/dogeTikz.js
+++ scripts/dogeTikz.js
@@ -50,6 +50,18 @@ const execPromise = cmd => new Promise( (resolve, reject) => {
   });
 });
 
+const laTexCreationFlow = (msg) => {
+  return laTexToPDF()
+  .then(pdfToJPG)
+  .then(jpgToBase64)
+  .then(imgInBase64 => compose(HTTPrequest, buildPost)(imgInBase64))
+  .then(response => msg.reply(`Here is your tikz rendering: ${response.data.link}`))
+  .catch(error => {
+    console.log('// err //', error);
+    msg.reply('There seems to be an error with the provided tikz-cd code or how it was processed:\n \n' + error + '\n \n in the event you have already tested this tikz-cd on your own and the was working, please notify the bot creator @iant')
+  })
+}
+
 const laTexToPDF = () => execPromise('pdflatex laTexFile.tex')
 const pdfToJPG = () => execPromise('convert -density 300 laTexFile.pdf -quality 90 laTexFile.jpg')
 const jpgToBase64 = () => execPromise('openssl base64 -in laTexFile.jpg')
@@ -64,15 +76,8 @@ module.exports = (robot) => {
 
     addBoilerAndCreateFile(parseRemoveDirect(msg.message.text))
 
-    laTexToPDF()
-    .then(pdfToJPG)
-    .then(jpgToBase64)
-    .then(imgInBase64 => compose(HTTPrequest, buildPost)(imgInBase64))
-    .then(response => msg.reply(`Here is your tikz rendering: ${response.data.link}`))
-    .catch(error => {
-      console.log('// err //', error);
-      msg.reply('There seems to be an error with the provided tikz-cd code or how it was processed:\n \n' + error + '\n \n in the event you have already tested this tikz-cd on your own and the was working, please notify the bot creator @iant')
-    })
+    laTexCreationFlow(msg)
+
   })
 
   robot.respond(/(tikz create)/i, function (msg) {
@@ -86,15 +91,7 @@ module.exports = (robot) => {
 
       addBoilerAndCreateFile(parseRemoveDoge(msg.message.text))
 
-      laTexToPDF()
-      .then(pdfToJPG)
-      .then(jpgToBase64)
-      .then(imgInBase64 => compose(HTTPrequest, buildPost)(imgInBase64))
-      .then(response => msg.reply(`Here is your tikz rendering: ${response.data.link}`))
-      .catch(error => {
-        console.log('// err //', error);
-        msg.reply('There seems to be an error with the provided tikz-cd code or how it was processed:\n \n' + error + '\n \n in the event you have already tested this tikz-cd on your own and the was working, please notify the bot creator @iant')
-      })
+      laTexCreationFlow(msg)
 
       awaitingTikzCode = false
     }
