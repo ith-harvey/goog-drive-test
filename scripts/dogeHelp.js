0bc59fc4e09096520fbf8eca46314df34bb9699f
@ianh <ith.harvey@gmail.com>
2018-05-30 17:53:17 +0200

    updated

--- scripts/dogeHelp.js
+++ scripts/dogeHelp.js
@@ -7,7 +7,9 @@ const Helptxt = {
 
   tikz: () => 'The `@doge tikz create` bot is a `tikz-cd` processing tool that takes in tikz-cd code, transforms the code into a jpg then into a base64 image. The image is then uploaded to imgur.com and the user is provided a link to the image.\n \n The commands are as follows: \n \n `@doge tikz create` : provides user boilerplate instructions and instructs doge to receive and process your tikz code. \n \n `@doge tikz direct` : immediatley receives and processes your tikz code all in one command (NOTE: the tikz code should be pasted on a new line directly below the `@doge tikz direct` command). \n \n If you are still having trouble shoot a DM to the creator `@iant`.',
 
-  basic: () => 'I\'m here to help! \n \n To get more specific information regarding which feature of the `@doge` bot you are having trouble with please run one of the following commands: \n \n`@doge help cal` : help with the calendar bot \n \n`@doge help tikz` : help with the tikz-cd bot \n \n If you are still having trouble send a DM to the creator `@iant`.'
+  expense: () => 'The `@doge expense create` bot is an expense processing tool that takes in a user\'s expense and posts their expense to a google sheet managed by Maker\'s accounting team. \n \n The commands are as follows: \n \n `@doge expense create` : provides user a list of viable expense catagories and the format with which to submit the expense. \n \n If you are still having trouble shoot a DM to the creator `@iant`.',
+
+  basic: () => 'I\'m here to help! \n \n To get more specific information regarding which feature of the `@doge` bot you are having trouble with please run one of the following commands: \n \n`@doge help cal` : help with the calendar bot \n \n`@doge help tikz` : help with the tikz-cd bot \n \n`@doge help expense` : help with the expense bot \n \n If you are still having trouble send a DM to the creator `@iant`.'
 
 }
 const purifyArray = array => array.slice()
@@ -20,6 +22,8 @@ const parseCommand = cmdArray => {
     return Helptxt.cal()
   } else if (cmdArray[1] === 'tikz') {
     return Helptxt.tikz()
+  } else if (cmdArray[1] === 'expense') {
+    return Helptxt.expense()
   } else {
     return Helptxt.basic()
   }
